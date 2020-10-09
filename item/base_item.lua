930d296607255ddecd8000a0936ec079f3ea7e69
JiangZhiwei <judgezerowit763@163.com>
2020-10-09 13:04:05 +0800

    修改错误`~~

--- item/base_item.lua
+++ item/base_item.lua
@@ -59,6 +59,11 @@ function BaseItem:useItem2 (objid)
   -- body
 end
 
+-- 选择道具
+function BaseItem:selectItem (objid)
+  -- body
+end
+
 -- 投掷物命中
 function BaseItem:projectileHit(projectileInfo, toobjid, blockid, pos)
   -- body
